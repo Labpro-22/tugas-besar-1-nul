559e5859f42b455d248705f9fc517c87e45c2275
Vixrlie <vinctlie06@gmail.com>
2026-04-16 14:32:37 +0700

    feat: template implementation lasso card

--- src/card/LassoCard.cpp
+++ src/card/LassoCard.cpp
@@ -1,9 +1,11 @@
 #include "LassoCard.hpp"
+#include "player/Player.h"
 
 LassoCard::LassoCard() : SkillCard("LassoCard: Menarik pemain lawan", 0) {}
 
-void LassoCard::use(Player* player, TurnContext& ctx) {
+void LassoCard::apply(Player& player) {
     std::cout << "[MENGGUNAKAN] LassoCard: Menarik pemain lawan ke posisi ini.\n";
+
     // TODO: Tampilkan daftar pemain lain yang posisinya berada di "depan"
     // TODO: Minta input pemain mana yang ditarik
     // TODO: Pindahkan pemain lawan tersebut ke petak player saat ini
