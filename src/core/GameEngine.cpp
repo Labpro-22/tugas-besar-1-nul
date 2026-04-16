cd32493547516922d3599f8751d2d5a756805282
Vixrlie <vinctlie06@gmail.com>
2026-04-17 03:54:30 +0700

    feat: minimal core classes (GameEngine and TurnContext)

---
+++ src/core/GameEngine.cpp
@@ -0,0 +1,24 @@
+#include "core/GameEngine.hpp"
+
+
+
+// ini pake pointer, tapi gameengine pake unique_ptr,
+// jdi mesti cek lagi apakah smuany pen diubah jdi unique_ptr
+// ato ga
+std::vector<Player*> GameEngine::getPlayers() const {
+    std::vector<Player*> result;
+    result.reserve(players.size());
+    for (const auto& player : players) {
+        result.push_back(player.get());
+    }
+    return result;
+}
+
+
+void GameEngine::displayPlayers() const {
+    std::cout << "\n=== Daftar Pemain ===\n";
+    for (const auto& player : players) {
+        std::cout << "Pemain: " << player->getUsername() << " | Posisi: " << player->getPosition() << " | Kekayaan: " << player->getWealth() << "\n";
+    }
+    std::cout << "=====================\n\n";
+}
