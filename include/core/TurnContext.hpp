e92a9f5528fc539554825a07d28ba36305bf069b
Vixrlie <vinctlie06@gmail.com>
2026-04-21 01:47:05 +0700

    refactor: pointer to ref type for RAII

--- include/core/TurnContext.hpp
+++ include/core/TurnContext.hpp
@@ -1,19 +1,20 @@
 #pragma once
 
+#include "core/Dice.hpp"
+
 class Player;
-class Dice;
 class Board;
 class GameEngine;
 
 class TurnContext {
 public:
-    Player* currentPlayer;
-    Dice* dice;
-    Board* board;
-    GameEngine* gameEngine;
+    Player& currentPlayer;
+    Dice& dice;
+    Board& board;
+    GameEngine& gameEngine;
     int diceTotal;
 
-    TurnContext(Player* player, Dice* dice, Board* board, GameEngine* gameEngine)
+    TurnContext(Player& player, Dice& dice, Board& board, GameEngine& gameEngine)
         : currentPlayer(player), dice(dice), board(board), gameEngine(gameEngine), diceTotal(0) {}
 
 };
