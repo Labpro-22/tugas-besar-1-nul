62c82b993cd5d8aad984fb6c187342a67e423118
Vixrlie <vinctlie06@gmail.com>
2026-04-20 20:07:13 +0700

    feat: gameengine ctor and implementations

--- src/core/GameEngine.cpp
+++ src/core/GameEngine.cpp
@@ -1,6 +1,64 @@
 #include "core/GameEngine.hpp"
+#include "core/TurnContext.hpp"
 
+void GameEngine::run() {
+    std::cout << "=== Welcome to Nimonspoli ===" << endl;
+    this->startNewGame();
 
+    
+
+    while (!turnmgr.isGameOver()) {
+        turnmgr.nextTurn();
+        TurnContext ctx(turnmgr.getCurrentPlayer(), nullptr, &board, this);
+        Player* currentPlayer = turnmgr.getCurrentPlayer();
+        std::cout << "\n=== Giliran " << turnmgr.getCurrentTurn() << ": " << currentPlayer->getUsername() << " ===\n";
+        
+        // TO-DO: player decide action
+    }
+
+}
+
+void GameEngine::loadGame(const std::string& file) {
+    std::cout << "[INFO] Memuat konfigurasi board dari file: " << file << "\n";
+    this->board = Board(40); // coba default dulu
+
+    int maxTurns;
+    std::cout << "[INFO] Memuat giliran maksimal permainan\n";
+    std::cout << "Masukkan batas jumlah giliran permainan (atau -1 untuk tanpa batas)\n";
+    std::cout << "> ";
+    std::cin >> maxTurns;
+    std::cin.ignore();
+    this->turnmgr = TurnManager(maxTurns);
+
+    int numPlayers;
+    std::cout << "[INFO] Memuat pemain\n";
+    std::cout << "Masukkan jumlah pemain (2-6)\n";
+    std::cout << "> ";
+    std::cin >> numPlayers;
+    std::cin.ignore();
+    for (int i = 0; i < numPlayers; ++i) {
+        std::string username;
+        std::cout << "Masukkan nama pemain " << (i + 1) << ": ";
+        std::getline(std::cin, username);
+        players.push_back(std::make_unique<Player>(username, 1500)); // saldo awal 1500
+    }
+    turnmgr.setTurnOrder(this->getPlayers());
+
+}
+
+void GameEngine::startNewGame() {
+    std::cout << "[INFO] Memulai permainan baru...\n";
+    std::cout << "Masukkan name file config board (atau tekan Enter untuk default)\n" ;
+    std::cout << "> ";
+    std::cin.ignore();
+    std::string filename;
+    std::getline(std::cin, filename);
+    if (filename.empty()) {
+        this->loadGame("default_config.txt");
+    } else {
+        this->loadGame(filename);
+    }
+}
 
 // ini pake pointer, tapi gameengine pake unique_ptr,
 // jdi mesti cek lagi apakah smuany pen diubah jdi unique_ptr
@@ -21,4 +79,9 @@ void GameEngine::displayPlayers() const {
         std::cout << "Pemain: " << player->getUsername() << " | Posisi: " << player->getPosition() << " | Kekayaan: " << player->getWealth() << "\n";
     }
     std::cout << "=====================\n\n";
+}
+
+void GameEngine::executeCommand(const std::string& cmd) {
+    std::cout << "[INFO] Eksekusi perintah: " << cmd << "\n";
+    // TO-DO: mikir input tuh mau dimasukkin dimana
 }
