26234f35e9a9a3c544203f33de21162850b9cf17
Kurt-Mikhael <kurtmikhael123@gmail.com>
2026-04-20 15:24:48 +0700

    Feat: bankruptcy manager hpp

---
+++ include/core/BankruptcyManager.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "property/Property.hpp"
+
+class Player;
+
+// Struct to represent a liquidation option
+struct LiquidationOption {
+    enum Type { SELL, MORTGAGE };
+
+    Property* property;
+    Type type;
+    int cashValue;  // Cash received if executed
+
+    LiquidationOption(Property* prop, Type t, int value)
+        : property(prop), type(t), cashValue(value) {}
+};
+
+class BankruptcyManager {
+  public:
+
+    static int calculateMaxLiquidationFunds(Player* player);
+    static int calculateSellValue(Property* property);
+    static bool canCoverDebt(Player* player, int debtAmount);
+    static std::vector<LiquidationOption> getAvailableLiquidationOptions(Player* player);
+    static void performForcedLiquidation(Player* player, int debtAmount);
+    static void declareBankrupt(Player* player);
+};
