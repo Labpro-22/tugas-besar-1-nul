7a8933f410d0d79db4121d7967cdfbc4a062f00d
Kurt-Mikhael <kurtmikhael123@gmail.com>
2026-04-22 19:38:06 +0700

    Auction manager

---
+++ include/core/AuctionManager.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vector>
+
+class Player;
+class Property;
+
+class AuctionManager {
+  public:
+
+    Player* runAuction(Property* prop,
+                       const std::vector<Player*>& players,
+                       int startIdx);
+
+    int getLastWinningBid() const;
+
+  private:
+    int lastWinningBid_ = -1;
+};
