9090e531e9c9b780411110b792500e91a7c6ddd8
Vixrlie <vinctlie06@gmail.com>
2026-04-24 15:14:28 +0700

    feat: triggerBankruptcy

--- include/tile/PropertyTile.hpp
+++ include/tile/PropertyTile.hpp
@@ -30,6 +30,7 @@ class StreetTile : public PropertyTile{
         void onLanded(TurnContext& ctx) override;
         void triggerBuyOrAuction(TurnContext& ctx);
         void triggerRentPayment(TurnContext& ctx);
+        bool triggerBankruptcy(TurnContext& ctx, int debtAmount);
 };
 
 class RailroadTile : public PropertyTile{
